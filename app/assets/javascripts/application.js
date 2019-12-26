f78aaf8904fb8dc0adb90031bad1f13ce99f7dae
sumancrest0001 <suman.crest0001@gmail.com>
2019-12-26 18:47:10 +0545

    navigation bar and favicon added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require turbolinks
 //= require_tree .
+//= require semantic-ui
+$(document).on('turbolinks:load', function(){
+  $('.ui.dropdown').dropdown();
+})
